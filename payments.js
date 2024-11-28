d64af6bd8adcf39e56270d53aa318f4e24d6b1ad
Manikandan <138711523+Manikandan2409@users.noreply.github.com>
2024-11-28 14:57:06 +0000

    adding some in payments

--- payments.js
+++ payments.js
@@ -47,5 +47,5 @@ app.post('/verify-payment', (req, res) => {
 
 // Start server
 app.listen(5000, () => {
-  console.log('Server running on http://localhost:5000');
+  console.log('Server running ');
 });
